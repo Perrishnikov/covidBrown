9fc7f3ea699f6503683e62cc364b0a5f1e8092f6
perrishnikov <perrishnikov@gmail.com>
2020-06-12 20:15:09 -0500

    module fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-import fetchData from '/scripts/fetchData.js';
-import chartAttack from '/scripts/chartAttack.js';
+import fetchData from './scripts/fetchData.js';
+import chartAttack from './scripts/chartAttack.js';
 // import { parseData } from '/scripts/fetchData.js';
 
 
